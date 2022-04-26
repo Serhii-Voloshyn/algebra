be7961994b8f3c15aabf5ea751276ab79ea6d782
Сергій Волошин <serhii.voloshyn59@gmail.com>
2022-04-26 22:11:45 +0300

    improved readibility

--- oleksii_tests/test_tasks.py
+++ oleksii_tests/test_tasks.py
@@ -1,10 +1,14 @@
+"""Testing tasks"""
+
 import unittest
 import tasks
 
 
 class TestTasks(unittest.TestCase):
+    """Testing tasks package"""
 
     def test_task_88v(self):
+        """Testing task_88v function"""
         self.assertEqual(tasks.task_88v(371239), 971233)
         self.assertEqual(tasks.task_88v(5), 5)
 
@@ -23,6 +27,7 @@ class TestTasks(unittest.TestCase):
 
 
     def test_task_88g(self):
+        """Testing task_88g function"""
         self.assertEqual(tasks.task_88g(371239), 13712391)
         self.assertEqual(tasks.task_88g(5), 151)
 
@@ -41,6 +46,7 @@ class TestTasks(unittest.TestCase):
 
 
     def test_task_332(self):
+        """Testing task_332 function"""
         self.assertCountEqual(tasks.task_332(31), (5, 1, 2, 1))
 
         with self.assertRaises(ValueError):
